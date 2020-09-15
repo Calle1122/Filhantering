5b926dbb9601571993299f774a81f97593fa67ba
Calle <carl@lindstedt.net>
2020-09-15 13:28:28 +0200

    Man kan nu skriva in variabler för spelaren och sedan sparas det i en XML-fil.

---
+++ Filhantering/Player.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Filhantering
+{
+    public class Player
+    {
+        public string name;
+        public int hp;
+        public int atk;
+        public int def;
+    }
+}
--- Filhantering/Program.cs
+++ Filhantering/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Xml.Serialization;
 
 namespace Filhantering
 {
@@ -6,7 +8,41 @@ namespace Filhantering
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Player myPlayer = new Player();
+
+            System.Console.WriteLine("What is the Players name?");
+            string name = Console.ReadLine(); 
+            myPlayer.name = name;
+
+            System.Console.WriteLine("What is the Players hp?");
+            string hp = Console.ReadLine();
+            int hpInt = int.Parse(hp);
+            myPlayer.hp = hpInt;
+
+            System.Console.WriteLine("What is the Players attack?");
+            string atk = Console.ReadLine();
+            int atkInt = int.Parse(atk);
+            myPlayer.atk = atkInt;
+
+            System.Console.WriteLine("What is the Players defense?");
+            string def = Console.ReadLine();
+            int defInt = int.Parse(def);
+            myPlayer.def = defInt;
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Player));
+
+            FileStream file = File.Open("player.xml", FileMode.OpenOrCreate);
+
+            serializer.Serialize(file, myPlayer);
+
+            file.Close();
+
+
+
+            // myPlayer = (Player) serializer.Deserialize(file);
+
+            // Console.ReadLine();
         }
     }
 }
