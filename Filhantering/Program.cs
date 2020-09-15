772f4588f61cd20c6d8cb9832ab60452e2969d79
Calle <carl@lindstedt.net>
2020-09-15 13:52:54 +0200

    Man kan nu välja ifall man vill skapa en ny instans av Player eller ifall man vill läsa en tidigare.

--- Filhantering/Program.cs
+++ Filhantering/Program.cs
@@ -8,41 +8,61 @@ namespace Filhantering
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine("Vill du skapa en ny spelare eller läsa en gammal?");
+            System.Console.WriteLine("Skriv 'ny' eller 'gammal'");
+            string svar = Console.ReadLine();
 
-            Player myPlayer = new Player();
+            if(svar == "ny")
+            {
+                Player myPlayer = new Player();
 
-            System.Console.WriteLine("What is the Players name?");
-            string name = Console.ReadLine(); 
-            myPlayer.name = name;
+                System.Console.WriteLine("What is the Players name?");
+                string name = Console.ReadLine(); 
+                myPlayer.name = name;
 
-            System.Console.WriteLine("What is the Players hp?");
-            string hp = Console.ReadLine();
-            int hpInt = int.Parse(hp);
-            myPlayer.hp = hpInt;
+                System.Console.WriteLine("What is the Players hp?");
+                string hp = Console.ReadLine();
+                int hpInt = int.Parse(hp);
+                myPlayer.hp = hpInt;
 
-            System.Console.WriteLine("What is the Players attack?");
-            string atk = Console.ReadLine();
-            int atkInt = int.Parse(atk);
-            myPlayer.atk = atkInt;
+                System.Console.WriteLine("What is the Players attack?");
+                string atk = Console.ReadLine();
+                int atkInt = int.Parse(atk);
+                myPlayer.atk = atkInt;
 
-            System.Console.WriteLine("What is the Players defense?");
-            string def = Console.ReadLine();
-            int defInt = int.Parse(def);
-            myPlayer.def = defInt;
+                System.Console.WriteLine("What is the Players defense?");
+                string def = Console.ReadLine();
+                int defInt = int.Parse(def);
+                myPlayer.def = defInt;
 
-            XmlSerializer serializer = new XmlSerializer(typeof(Player));
+                XmlSerializer serializer = new XmlSerializer(typeof(Player));
 
-            FileStream file = File.Open("player.xml", FileMode.OpenOrCreate);
+                FileStream file = File.Open("player.xml", FileMode.OpenOrCreate);
 
-            serializer.Serialize(file, myPlayer);
+                serializer.Serialize(file, myPlayer);
 
-            file.Close();
+                file.Close();
+            }
 
+            else if(svar == "gammal")
+            {
+                Player myPlayer = new Player();
 
+                XmlSerializer serializer = new XmlSerializer(typeof(Player));
 
-            // myPlayer = (Player) serializer.Deserialize(file);
+                FileStream file = File.Open("player.xml", FileMode.OpenOrCreate);
 
-            // Console.ReadLine();
+                myPlayer = (Player) serializer.Deserialize(file);
+
+                file.Close();
+
+                System.Console.WriteLine(myPlayer.name);
+                System.Console.WriteLine(myPlayer.hp);
+                System.Console.WriteLine(myPlayer.atk);
+                System.Console.WriteLine(myPlayer.def);
+
+                Console.ReadLine();
+            }
         }
     }
 }
